a698fd0bb4366b7fae6da27467c26225244bd25d
Administrator <admin@example.com>
2022-09-15 17:23:08 +0800

    feat: sideNav

--- src/layout/components/SideNav.vue
+++ src/layout/components/SideNav.vue
@@ -12,8 +12,8 @@ const isActive = ref<boolean>(true)
         <h2>ECARRY</h2>
       </a>
       <div class="sidenav-btn" @click="isActive = !isActive">
-        <svg v-if="isActive"  width="20" height="20" viewBox="0 0 24 24"><g fill="#ffffff"><path d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z"/><path fill-rule="evenodd" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10Zm-2 0a8 8 0 1 1-16 0a8 8 0 0 1 16 0Z" clip-rule="evenodd"/></g></svg>
-        <svg v-else width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" fill-rule="evenodd" d="M12 20a8 8 0 1 0 0-16a8 8 0 0 0 0 16Zm0 2c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Z" clip-rule="evenodd"/></svg>
+        <svg v-if="isActive"  width="20" height="20" viewBox="0 0 24 24"><g fill="#ffffffC2"><path d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z"/><path fill-rule="evenodd" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10Zm-2 0a8 8 0 1 1-16 0a8 8 0 0 1 16 0Z" clip-rule="evenodd"/></g></svg>
+        <svg v-else width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffffC2" fill-rule="evenodd" d="M12 20a8 8 0 1 0 0-16a8 8 0 0 0 0 16Zm0 2c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Z" clip-rule="evenodd"/></svg>
       </div>
     </div>
     <div class="menu-wrap">
