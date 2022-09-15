6ea5b98c29b22f425bef8fedc48e8bee0e48d7a2
Administrator <admin@example.com>
2022-09-15 16:40:38 +0800

    feat: sideNav

---
+++ src/layout/components/MenuContent.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="menu-content">
+    <div class="menu-list">
+      <div class="menu-item">
+        <div class="menu-icon"><svg width="24" height="24" viewBox="0 0 16 16"><path fill="#ffffff" fill-rule="evenodd" d="m8.36 1.37l6.36 5.8l-.71.71L13 6.964v6.526l-.5.5h-3l-.5-.5v-3.5H7v3.5l-.5.5h-3l-.5-.5V6.972L2 7.88l-.71-.71l6.35-5.8h.72zM4 6.063v6.927h2v-3.5l.5-.5h3l.5.5v3.5h2V6.057L8 2.43L4 6.063z" clip-rule="evenodd"/></svg></div>
+        <div class="menu-title">Dashboards</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.menu-content {
+  .menu-list{
+    .menu-item {
+      display: flex;
+      align-items: center;
+      height: 44px;
+      color: #ffffffc2;
+      padding: 0 18px 0 22px;
+      cursor: pointer;
+
+      background-image: linear-gradient(98deg, #c48eff 0%, #9155fd 94%);
+      border-bottom-right-radius: 22px;
+      border-top-right-radius: 22px;
+      .menu-icon {
+        margin-right: 12px;
+      }
+      .menu-title {
+        line-height: 44px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/layout/components/SideNav.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import MenuContent from './MenuContent.vue'
+
+const isActive = ref<boolean>(true)
+</script>
+
+<template>
+  <div class="sidebar">
+    <div class="logo-wrap">
+      <a href="#">
+        <img src="../../assets/images/logo.png" alt="">
+        <h2>ECARRY</h2>
+      </a>
+      <div class="sidenav-btn" @click="isActive = !isActive">
+        <svg v-if="isActive"  width="20" height="20" viewBox="0 0 24 24"><g fill="#ffffff"><path d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z"/><path fill-rule="evenodd" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10Zm-2 0a8 8 0 1 1-16 0a8 8 0 0 1 16 0Z" clip-rule="evenodd"/></g></svg>
+        <svg v-else width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" fill-rule="evenodd" d="M12 20a8 8 0 1 0 0-16a8 8 0 0 0 0 16Zm0 2c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Z" clip-rule="evenodd"/></svg>
+      </div>
+    </div>
+    <div class="menu-wrap">
+      <MenuContent/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  width: 100%;
+  height: 100%;
+  .logo-wrap {
+    padding: 20px 20px 8px 24px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a {
+      display: flex;
+      img {
+        width: 30px;
+        object-fit: cover;
+        margin-right: 12px;
+      }
+      h2 {
+        font-size: 1.25rem;
+        font-weight: 700;
+        letter-spacing: .3px;
+        color: #fff;
+      }
+    }
+    svg {
+      cursor: pointer;
+    }
+  }
+  .menu-wrap {
+    padding: 8px 20px 8px 0;
+  }
+}
+</style>
